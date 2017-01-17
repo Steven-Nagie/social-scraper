e381aabe955cd4111d9384b919bb95fc35c1b7b6
Jon Myrick <jonmyrick7401@gmail.com>
2017-01-16 18:38:46 -0700

    added twitter api

--- processes/apiCtrl.js
+++ processes/apiCtrl.js
@@ -1,4 +1,15 @@
-const q = require('q');
+const q = require('q'),
+      Twit = require('twit'),
+      config = require('./../config');
+
+var twitter = new Twit(config.twitter)
+
+
+
+
+
+
+
 
 module.exports = {
   getFacebookProfile: function(data){
@@ -19,13 +30,11 @@ module.exports = {
 
     return defered.promise
   },
-  getTwitterProfile: function(data){
+  getTwitterProfile: function(url){
     let defered = q.defer()
-    // here we do a q.all do get all profile information. 
-    setTimeout(function(){
-      defered.resolve(`${data} then got profile`);
-    }, 500)
-
+    twitter.get('statuses/show', { id: '804000988604399616'}, function (err, data, response) {
+      defered.resolve(data);
+    })
     return defered.promise
   }
 }//end of module.exports
