be4682f5aa5a00ac9d6026019acb7e3eb6344bac
Steven Nagie <sanagie@yahoo.com>
2017-01-19 22:22:21 -0700

    Console logging likes, comments, followers, and shares.

--- processes/fbApi.js
+++ processes/fbApi.js
@@ -3,11 +3,11 @@
 const config = require('./../config'),
       FB = require('fb');
 
-module.exports = {
+// module.exports = {
   facebook = (data) => {
       var accessToken;
       FB.options({version: 'v2.8'});
-      var app = FB.extend({appId: config.appId, appSecret: config.appSecret});
+      var app = FB.extend({appId: config.facebook.appId, appSecret: config.facebook.appSecret});
 
 
       // If there is any way to see the post's content, it would probably be easier to get its id from the user's feed than to parse the url looking for the correct id.
@@ -37,7 +37,6 @@ module.exports = {
           } else if (site.charAt(i) === '.' && userFlag) {
             dotCount++;
             if (dotCount === 3) {
-              console.log(dotCount);
               startSliceId = i + 1;
             }
           } else if (site.charAt(i) === '/' && i > startSliceId) {
@@ -53,44 +52,37 @@ module.exports = {
 
       parseUser();
 
-
-      // The id is in the URL, but formatted differently there than they want it here.
-      getPost = () => {
-        app.api(`${id}`, function(res) {
-          if(!res || res.error) {
-           console.log(!res ? 'error occurred' : res.error);
-           return;
-          }
-          console.log(res);
-          // var totalLikes = res.data.length;
-          // console.log("total likes: ", totalLikes);
-        });
-      }
-
-      // Get post shares
+      // Get post shares. Not quite sure what exactly the format will be like.
       getPostShares = () => {
         app.api(`${id}?fields=shares`, function(res) {
           if(!res || res.error) {
            console.log(!res ? 'error occurred' : res.error);
            return;
           }
-          console.log(res);
+          var shares;
+          if (res.shares) {
+            shares = res.shares.count;
+          } else {
+            shares = 0;
+          }
+          console.log("shares: ", shares);
         });
       }
 
       // Get post likes. Current limit set to 3000, which is an arbitrary number I chose.
-      getPostShares = () => {
-        app.api(`${id}/likes?limit=3000`, function(res) {
+      getPostLikes = () => {
+        app.api(`${id}/likes?limit=30000`, function(res) {
           if(!res || res.error) {
            console.log(!res ? 'error occurred' : res.error);
            return;
           }
-          console.log(res);
+          var likes = res.data.length;
+          console.log("Likes: ", likes);
         });
       }
 
       // Get post reactions. We can parse them out.
-      getPostLikes = () => {
+      getPostReactions = () => {
         app.api(`${id}/reactions`, function(res) {
           if(!res || res.error) {
            console.log(!res ? 'error occurred' : res.error);
@@ -100,73 +92,50 @@ module.exports = {
         });
       }
 
+      // Get post comments
+      getPostComments = () => {
+        app.api(`${id}/comments`, function(res) {
+          if(!res || res.error) {
+           console.log(!res ? 'error occurred' : res.error);
+           return;
+          }
+          var comments = 0;
+          if (res.data) {
+            comments = res.data.length;
+          }
+          console.log("comments: ", comments);
+        });
+      }
+
 
       // Try to get a person's public page
       // Hitting /likes will show us what they like, not how many likes they have.
       // ?fields=fan_count gets us how many likes they have.
       getPublicProfile = () => {
-        app.api(`${user}?fields=fan_count&limit=3000`, function(res) {
+        app.api(`${user}?fields=fan_count&limit=30000`, function(res) {
           if(!res || res.error) {
            console.log(!res ? 'error occurred' : res.error);
            return;
           }
-          console.log(res);
-          // var totalLikes = res.data.length;
-          // console.log("total likes: ", totalLikes);
+          console.log("profile fan count: ", res.fan_count);
         });
       }
 
-      // Getting likes for a specific photo from the page. Have to access the page
-      getPagePhotoLikes = (photoId) => {
-        app.api(`${photoId}/likes?limit=3000`, function(res) {
-          if(!res || res.error) {
-           console.log(!res ? 'error occurred' : res.error);
-           return;
-          }
-          console.log(res);
-          var totalLikes = res.data.length;
-          console.log("total likes: ", totalLikes);
-        });
-      }
-
-      // Grabbing a page's photos, using an album id that we get from getPage();
-      getPagePhotos = (albumId) => {
-        app.api(`${albumId}/photos`, function(res) {
-          if(!res || res.error) {
-           console.log(!res ? 'error occurred' : res.error);
-           return;
-          }
-          // console.log(res);
-          getPagePhotoLikes(res.data[0].id)
-        });
-      }
-
-      // Grabbing a page based on id, then grabbing their albums
-      getPage = () => {
-        app.api(`19292868552/albums`, function(res) {
-          if(!res || res.error) {
-           console.log(!res ? 'error occurred' : res.error);
-           return;
-          }
-          // console.log(res);
-          getPagePhotos(res.data[1].id)
-        });
-      }
 
       // To generate an App access token
-      FB.api(`oauth/access_token?client_id=${config.appId}&client_secret=${config.appSecret}&grant_type=client_credentials`, function(res) {
+      FB.api(`oauth/access_token?client_id=${config.facebook.appId}&client_secret=${config.facebook.appSecret}&grant_type=client_credentials`, function(res) {
 
         if (!res || res.error) {
           console.log(!res ? 'error occurred' : res.error);
           return;
         }
 
-        accessToken = res.access_token;
-        console.log("access token: ", accessToken);
-        app.setAccessToken(accessToken);
-        // getPage();
+        app.setAccessToken(res.access_token);
         getPublicProfile();
-        getPost();
+        getPostLikes();
+        getPostShares();
+        getPostComments();
       })
     }
-  }
+  // }
+  facebook("https://www.facebook.com/brandonmikesell23/photos/a.841942249259190.1073741828.839057202881028/1144637245656354/?type=3&theater");
