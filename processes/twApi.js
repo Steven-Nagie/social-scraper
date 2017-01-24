bc2fa59388d0530c89ab0ad3d1a04db561d730e1
Jon Myrick <jonmyrick7401@gmail.com>
2017-01-23 22:34:41 -0700

    separates profile api calls and post api calls

--- processes/twApi.js
+++ processes/twApi.js
@@ -2,15 +2,29 @@ const q = require('q'),
       Twit = require('twit'),
       config = require('./../config');
 
-var twitter = new Twit(config.twitter)
+let twitter = new Twit(config.twitter)
 
 // Have to fill in the object below with the proper info, either from the api or from scraping.
 exports.getTwitterProfile = function(url){
   let defered = q.defer()
-  twitter.get('statuses/show', { id: '804000988604399616'}, function (err, data, response) {
-    var info = {username: data.user.screen_name, actualName: data.user.name, fanCount: data.user.followers_count, postLikes: , postShares: , retweets: data.retweet_count, replies: ,}
-    defered.resolve(info);
-  })
-
+  let id = url.substring(url.lastIndexOf('/') + 1);
+  if(Number(id)){
+    twitter.get('statuses/show', {id: id}, function (err, data, response) {
+      if (err) return console.log(err)
+      return defered.resolve({
+        username: data.user.screen_name, 
+        actualName: data.user.name, 
+        fanCount: data.user.followers_count, 
+        postLikes: data.favorite_count, 
+        retweets: data.retweet_count, 
+        postShares: 'Cannot get, I googled it', 
+        replies: 'Cannot get, I googled it'
+      });
+    })
+  }
+  else{
+    console.log('This is a profile', id)
+  }
+  
   return defered.promise
 };
