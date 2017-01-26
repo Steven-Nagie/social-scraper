46e73c3ca1331ecddad8d85474eb7c1acc61e5d6
Steven Nagie <sanagie@yahoo.com>
2017-01-26 15:20:37 -0700

    Created a table each for ig, tw, and fb. About to mess with promise ouput to format tables more easily.

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -11,7 +11,9 @@
       vm.startProcess = startProcess;
       vm.runTests = runTests;
       vm.login = login;
-      vm.csv = [{"This is" : "the output area"}];
+      vm.fbcsv = [{"This is" : "the output area"}];
+      vm.twcsv = [{"This is" : "the output area"}];
+      vm.igcsv = [{"This is" : "the output area"}];
       vm.fbProfiles = "";
       vm.testInput = 'https://twitter.com/highsteph/status/804000988604399616\nhttps://www.instagram.com/p/BNjEF8DA_-M/?taken-by=aimee_fuller\nhttps://twitter.com/JamieAsnow\nhttps://www.facebook.com/Jamieandersonsnow/photos/a.211038416173.137482.208209591173/10153945604136174/?type=3&theater\nhttps://www.instagram.com/p/BNhux3Ngj7O/?taken-by=jimmy_chin\nhttps://www.instagram.com/p/BNXVDlXBfnj/?taken-by=shawnjohnson\nhttps://twitter.com/ShawnJohnson/status/804370878599430144\nhttps://www.instagram.com/p/BNhux3Ngj7O/?taken-by=jimmy_chin\nhttps://twitter.com/jimkchin/status/806134951926067200\nhttps://www.instagram.com/p/BNUTi3Vghki/?taken-by=jebcorliss\nhttps://www.facebook.com/aimeefullersnow/\nhttps://www.instagram.com/p/BNZ1e5_gqcA/\nhttps://www.facebook.com/jeb.corliss/?fref=ts\nhttps://www.facebook.com/brandonmikesell23/photos/a.841942249259190.1073741828.839057202881028/1144637245656354/?type=3&theater'
 
@@ -24,7 +26,7 @@
         $scope.$apply(function () {
           console.log(profile);
           vm.fbProfiles += profile;
-          vm.csv = readCSV(vm.fbProfiles);
+          vm.fbcsv = readCSV(vm.fbProfiles);
         });
       });
 
@@ -79,8 +81,8 @@
         })
         return csv;
       }
-      vm.csv = readCSV(vm.fbProfiles);
-      console.log("csv ", vm.csv);
+      vm.fbcsv = readCSV(vm.fbProfiles);
+      console.log("csv ", vm.fbcsv);
 
 
     }; //End of MainCtrl
