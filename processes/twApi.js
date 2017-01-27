186cd9e0fe6f608262427b5f14704c7e62d4e7ab
Steven Nagie <sanagie@yahoo.com>
2017-01-26 17:36:02 -0700

    Broke

--- processes/twApi.js
+++ processes/twApi.js
@@ -1,7 +1,6 @@
 const q = require('q'),
       Twit = require('twit'),
-      config = require('./../config')
-      fs = require('fs');
+      config = require('./../config');
 
 let twitter = new Twit(config.twitter)
 
@@ -9,22 +8,10 @@ let twitter = new Twit(config.twitter)
 exports.getTwitterProfile = url => {
   let defered = q.defer()
   let id = url.substring(url.lastIndexOf('/') + 1);
-
-  let csvContent = '';
-  csvParse = (profile) => {
-    var totalObjLength = 0;
-    for (key in profile) {
-      totalObjLength++;
-
-      csvContent += totalObjLength < Object.keys(profile).length ? profile[key] + ',' : profile[key] + ',' + '\n';
-    };
-    fs.appendFileSync('twitter.csv', csvContent, encoding="utf8");
-  }
-
   if(Number(id)){
     twitter.get('statuses/show', {id: id},  (err, data, response) => {
       if (err) return console.log(err)
-      var profile = {
+      return defered.resolve({
         type: 'post',
         screen_name: data.user.screen_name,
         name: data.user.name,
@@ -32,25 +19,19 @@ exports.getTwitterProfile = url => {
         statuses_count: data.user.statuses_count,
         favorite_count: data.favorite_count,
         retweets: data.retweet_count
-      }
-      csvParse(profile);
-      console.log(csvContent);
-      return defered.resolve(csvContent);
+      });
     })
   }
   else{
     twitter.get('users/show', {iuser_id: '', screen_name: id}, (err, data, response) => {
       if (err) return console.log(err)
-      var profile = {
+      return defered.resolve({
         type: 'profile',
         screen_name: data.screen_name,
         name: data.name,
         followers_count: data.followers_count,
         statuses_count: data.statuses_count
-      }
-      csvParse(profile);
-      console.log(csvContent);
-      return defered.resolve(csvContent);
+      });
     })
   }
 
--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -15,8 +15,6 @@
       vm.twcsv = [{"This is" : "the output area"}];
       vm.igcsv = [{"This is" : "the output area"}];
       vm.fbProfiles = "";
-      vm.twProfiles = "";
-      vm.igProfiles = "";
       vm.testInput = 'https://twitter.com/highsteph/status/804000988604399616\nhttps://www.instagram.com/p/BNjEF8DA_-M/?taken-by=aimee_fuller\nhttps://twitter.com/JamieAsnow\nhttps://www.facebook.com/Jamieandersonsnow/photos/a.211038416173.137482.208209591173/10153945604136174/?type=3&theater\nhttps://www.instagram.com/p/BNhux3Ngj7O/?taken-by=jimmy_chin\nhttps://www.instagram.com/p/BNXVDlXBfnj/?taken-by=shawnjohnson\nhttps://twitter.com/ShawnJohnson/status/804370878599430144\nhttps://www.instagram.com/p/BNhux3Ngj7O/?taken-by=jimmy_chin\nhttps://twitter.com/jimkchin/status/806134951926067200\nhttps://www.instagram.com/p/BNUTi3Vghki/?taken-by=jebcorliss\nhttps://www.facebook.com/aimeefullersnow/\nhttps://www.instagram.com/p/BNZ1e5_gqcA/\nhttps://www.facebook.com/jeb.corliss/?fref=ts\nhttps://www.facebook.com/brandonmikesell23/photos/a.841942249259190.1073741828.839057202881028/1144637245656354/?type=3&theater'
 
 
@@ -26,31 +24,21 @@
 
       dataService.subscribeToFacebook(function (profile) {
         $scope.$apply(function () {
-          // console.log(profile);
+          console.log(profile);
           vm.fbProfiles += profile;
-          vm.fbcsv = readCSVFacebook(vm.fbProfiles);
+          vm.fbcsv = readCSV(vm.fbProfiles);
         });
       });
 
       dataService.subscribeToInstagram(function (profile) {
         $scope.$apply(function () {
-          console.log(profile);
-          vm.igProfiles += `${profile.data.type},${profile.data.user.username},${profile.data.user.full_name},${profile.data.comments.count},${profile.data.likes.count}\n`
-          vm.igcsv = readCSVInstagram(vm.igProfiles);
-          /*
-            - if video, include views
-            - comment count
-            - likes
-            - total followers
-          */
+          console.log(profile)
         });
       });
 
       dataService.subscribeToTwitter(function (profile) {
         $scope.$apply(function () {
-          // console.log(profile)
-          vm.twProfiles += profile;
-          vm.twcsv = readCSVTwitter(vm.twProfiles);
+          console.log(profile)
         });
       });
       /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
@@ -75,7 +63,7 @@
 
 
 
-      function readCSVFacebook(file) {
+      function readCSV(file) {
         let rows=file.split('\n');
         let cols = [];
         rows.forEach((row) => {
@@ -93,50 +81,8 @@
         })
         return csv;
       }
-      vm.fbcsv = readCSVFacebook(vm.fbProfiles);
-
-      function readCSVTwitter(file) {
-        let rows=file.split('\n');
-        let cols = [];
-        rows.forEach((row) => {
-          cols.push(row.split(','));
-        });
-        let csv = cols.map((row) => {
-          return {
-            "type": row[0],
-            "userName": row[1],
-            "name": row[2],
-            "followersCount": row[3],
-            "statusesCount": row[4],
-            "favoriteCount": row[5],
-            "retweets": row[6]
-          }
-        })
-        return csv;
-      }
-      vm.twcsv = readCSVTwitter(vm.twProfiles);
-
-      // `${profile.data.type},${profile.data.user.username},${profile.data.user.full_name},${profile.data.comments.count},${profile.data.likes.count}\n`
-
-      function readCSVInstagram(file) {
-        let rows=file.split('\n');
-        let cols = [];
-        rows.forEach((row) => {
-          cols.push(row.split(','));
-        });
-        let csv = cols.map((row) => {
-          return {
-            "type": row[0],
-            "userName": row[1],
-            "name": row[2],
-            "commentsCount": row[3],
-            "likesCount": row[4],
-            "videoViews": row[5]
-          }
-        })
-        return csv;
-      }
-      vm.igcsv = readCSVInstagram(vm.igProfiles);
+      vm.fbcsv = readCSV(vm.fbProfiles);
+      console.log("csv ", vm.fbcsv);
 
 
     }; //End of MainCtrl
