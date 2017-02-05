06271d12df41f43dfbc63c993d1a670c7c501133
Steven Nagie <sanagie@yahoo.com>
2017-02-05 11:26:27 -0700

    Changed ui-grid format, now working on getting data in proper format

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -11,12 +11,33 @@
       vm.startProcess = startProcess;
       vm.runTests = runTests;
       vm.login = login;
-      vm.fbcsv = [{"This is" : "the output area"}];
-      vm.twcsv = [{"This is" : "the output area"}];
-      vm.igcsv = [{"This is" : "the output area"}];
+      vm.csv = [{
+        "Twitter Link": "",
+        "Who Tweeted It": "",
+        "Followers (tw)": "",
+        "Likes (tw)": "",
+        "Shares (tw)": "",
+        "Retweets": "",
+        "Replies": "",
+        "Facebook Link": "",
+        "Followers (fb)": "",
+        "Likes (fb)": "",
+        "Shares (fb)": "",
+        "Comments (fb)": "",
+        "Instagram Link": "",
+        "Followers (ig)": "",
+        "Likes (ig)": "",
+        "Comments (ig)": "",
+        "Video Views (ig)": ""
+      }];
+      vm.csvError = [{
+        "Link": "",
+        "Error": ""
+      }];
       vm.fbProfiles = "";
       vm.twProfiles = "";
       vm.igProfiles = "";
+      vm.errorTableFlag = false;
       vm.testInput = 'https://twitter.com/highsteph/status/804000988604399616\nhttps://www.instagram.com/p/BNjEF8DA_-M/?taken-by=aimee_fuller\nhttps://twitter.com/JamieAsnow\nhttps://www.facebook.com/Jamieandersonsnow/photos/a.211038416173.137482.208209591173/10153945604136174/?type=3&theater\nhttps://www.instagram.com/p/BNhux3Ngj7O/?taken-by=jimmy_chin\nhttps://www.instagram.com/p/BNXVDlXBfnj/?taken-by=shawnjohnson\nhttps://twitter.com/ShawnJohnson/status/804370878599430144\nhttps://www.instagram.com/p/BNhux3Ngj7O/?taken-by=jimmy_chin\nhttps://twitter.com/jimkchin/status/806134951926067200\nhttps://www.instagram.com/p/BNUTi3Vghki/?taken-by=jebcorliss\nhttps://www.facebook.com/aimeefullersnow/\nhttps://www.instagram.com/p/BNZ1e5_gqcA/\nhttps://www.facebook.com/jeb.corliss/?fref=ts\nhttps://www.facebook.com/brandonmikesell23/photos/a.841942249259190.1073741828.839057202881028/1144637245656354/?type=3&theater'
 
 
@@ -73,15 +94,31 @@
       };
 
       /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
-                                CSV DISPLAY IN HTML STUFF
+                                CSV DISPLAY IN HTML STUFF (including ng-style)
       /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
+      vm.goodTable = {
+        "box-sizing":"border-box",
+        "color":"#333",
+        "font-size": "12px",
+        "border": "none",
+        "height": "calc(100vh - 15px)",
+        "width": "calc(100vw - 350px)",
+        "min-width": "500px"
+      }
+      vm.errorTable = {
+        "display": "none",
+        "font-size": "12px",
+        "border": "none",
+        "height": "20vh",
+        "width": "calc(100vw - 350px)",
+        "min-width": "500px"
+      }
+      if (vm.errorTableFlag) {
+        vm.goodTable["height"] = "calc(80vh - 15px)";
+        vm.errorTable["display"] = "block";
+      }
 
-      vm.fbcsv = csvService.readCSVFacebook(vm.fbProfiles);
-
-      vm.twcsv = csvService.readCSVTwitter(vm.twProfiles);
-
-      vm.igcsv = csvService.readCSVInstagram(vm.igProfiles);
 
       /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
                                           EXPORT
