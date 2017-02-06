f005583721b03d9e77c031ede65b0964842644ef
Steven Nagie <sanagie@yahoo.com>
2017-02-06 13:56:10 -0700

    Loading bar working perfectly

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -56,6 +56,7 @@
       vm.profilesArray = [];
       vm.countInput = 0;
       vm.countOutput = 0;
+      vm.percentLoaded = 0;
       vm.testInput = 'https://twitter.com/highsteph/status/804000988604399616\nhttps://www.instagram.com/p/BNjEF8DA_-M/?taken-by=aimee_fuller\nhttps://twitter.com/JamieAsnow\nhttps://www.facebook.com/Jamieandersonsnow/photos/a.211038416173.137482.208209591173/10153945604136174/?type=3&theater\nhttps://www.instagram.com/p/BNhux3Ngj7O/?taken-by=jimmy_chin\nhttps://www.instagram.com/p/BNXVDlXBfnj/?taken-by=shawnjohnson\nhttps://twitter.com/ShawnJohnson/status/804370878599430144\nhttps://www.instagram.com/p/BNhux3Ngj7O/?taken-by=jimmy_chin\nhttps://twitter.com/jimkchin/status/806134951926067200\nhttps://www.instagram.com/p/BNUTi3Vghki/?taken-by=jebcorliss\nhttps://www.facebook.com/aimeefullersnow/\nhttps://www.instagram.com/p/BNZ1e5_gqcA/\nhttps://www.facebook.com/jeb.corliss/?fref=ts\nhttps://www.facebook.com/brandonmikesell23/photos/a.841942249259190.1073741828.839057202881028/1144637245656354/?type=3&theater'
 
       vm.input = "";
@@ -75,13 +76,12 @@
       dataService.subscribeToData(function (profile) {
         $scope.$apply(function () {
           vm.countOutput += 1;
-          // console.log("Input: ", vm.countInput, "Output: ", vm.countOutput);
+          vm.loadingBarInner["width"] = ((vm.countOutput/vm.countInput) * 20) + "vw";
+          vm.percentLoaded = Math.round((vm.countOutput/vm.countInput) * 100);
+          console.log(vm.percentLoaded);
 
           vm.profilesArray.push(profile);
 
-
-
-          // Do we want to make it so that we continually update as we get new info, which would require that we sort the data even after it's been on the dom but will ensure that data is spit out continuously and irrelevant to errors, or do we want to sort before we do anything, which will be smoother but which means that the user won't see anything until everything is all done, and which means that if one of our inputs doesn't return anything we have a problem
           if (vm.countOutput === vm.countInput) {
             vm.profilesArray.sort(sortbyInput(vm.input));
             vm.profilesArray.forEach(function(profile) {
@@ -90,7 +90,6 @@
               } else if (profile.givenInput.includes('facebook')) {
                 vm.profilesString += csvService.parseProfileFacebook(profile);
               } else if (profile.givenInput.includes('twitter')) {
-                console.log(profile);
                 vm.profilesString += csvService.parseProfileTwitter(profile);
               } else if (profile.givenInput.includes('instagram')) {
                 vm.profilesString += csvService.parseProfileInstagram(profile);
@@ -103,19 +102,61 @@
               data: vm.csv,
               enableColumnResizing: true,
             }
+            vm.loading["display"] = "none";
           }
 
         });
       })
 
-      //rxjs to sort vm.csv as it changes
+      /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
+                                    LOADING CSS
+      /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+      vm.loading = {
+        "z-index": "100",
+        "background-color": "red",
+        "position": "absolute",
+        "display": "none",
+        "flex-direction": "column",
+        "justify-content": "space-between",
+        "align-items": "center",
+        "top": "50%",
+        "left": "50%",
+        "transform": "translate(-50%, -50%)",
+        "height": "30vh",
+        "width": "30vw",
+        "padding": "5vh 0 5vh 0",
+      }
 
+      vm.loadingBarOuter = {
+        "height": "3vh",
+        "width": "20vw",
+        "background-color": "blue"
+      }
+
+      vm.percentLoadedStyle = {
+        "position": "absolute",
+        "top": "50%",
+        "left": "50%",
+        "transform": "translate(-50%, -50%)"
+      }
+
+      vm.loadingBarInner = {
+        "height": "3vh",
+        "width": "0vw",
+        "float": "left",
+        "background-color": "green"
+      }
+
+      vm.loadingButton = {
+        "cursor": "pointer"
+      }
 
       /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
                                     USER INTERACTION
       /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
       function startProcess(postsURLs){
+        vm.loading["display"] = "flex";
         vm.input = postsURLs.split(/\r?\n/);
         vm.countInput = vm.input.length;
         // console.log(vm.input);
@@ -127,7 +168,26 @@
       };
 
       vm.getResultsNow = function() {
-        console.log("User wants results");
+        vm.profilesArray.sort(sortbyInput(vm.input));
+        vm.profilesArray.forEach(function(profile) {
+          if (profile.error) {
+            vm.profilesString += csvService.parseProfileError(profile);
+          } else if (profile.givenInput.includes('facebook')) {
+            vm.profilesString += csvService.parseProfileFacebook(profile);
+          } else if (profile.givenInput.includes('twitter')) {
+            vm.profilesString += csvService.parseProfileTwitter(profile);
+          } else if (profile.givenInput.includes('instagram')) {
+            vm.profilesString += csvService.parseProfileInstagram(profile);
+          } else {
+            vm.profilesString += csvService.parseProfileError(profile);
+          }
+        })
+        vm.csv = csvService.readCSV(vm.profilesString);
+        vm.gridOptions = {
+          data: vm.csv,
+          enableColumnResizing: true,
+        }
+        vm.loading["display"] = "none";
       }
 
 
