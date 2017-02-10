9e35ba4fe454813bca4a955b2c2da367ce8163e9
Jon Myrick <jonmyrick7401@gmail.com>
2017-02-10 08:43:34 -0700

    modified throttler to be more accurate

--- processes/throttler.js
+++ processes/throttler.js
@@ -1,45 +1,53 @@
-// more than 20 rps is too fast. 
+ 
+ var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+ var rps = 100
+ var errors = 5;
 
-var rps = 200
-var i = 0;
-var errors = 5;
+//The goal here is to gradually increase the speed untill we get a socket hangup. 
+//This method will take 5 socket hangups to find the fasted possible speed
+//It will take those errors and push the value back to the call array to be reran so no values are lost.
+//After the initial 5 hangups, the bot will reduce speed by 20ms for each additional hangup. 
+
 
 function speedOk(){
-  if(rps > 50) return true;
-  else return false
-}
-//if its a socket hangup, push the request back into the array to 
+   if (rps > 50) return true;
+   else return false
+ }
 
-function getRequest(){
-  console.log('rps = ', (1000 / rps).toFixed(2))
-  if(errors  > 0){
-    if(speedOk()) rps -= errors;
-    else {
-      console.log('error')
-      rps += 1
-      errors--
-    }
-  }
-  else if (errors === 5) {
-    rps += 5;
-    errors++
-  }
-}
+ function adjustSpeed() {
+   if (errors > 0) {
+     if (speedOk()) {
+       rps -= errors;
+       return true;
+     } else {
+       rps += 1
+       errors--
+       return false;
+     }
+   } else {
+     return true;
+   }
+ }
 
-var throttler = function(){
-    if(i < 100) {
-      
-        getRequest();
-        
-        setTimeout(function() {
-          throttler()
-        }, rps); 
-        
-        i++;
-    }
-}
+ function makeCalls(element, arr) {
+   if (adjustSpeed()) {
+     console.log(element)
+   } else {
+     console.log('error')
+     arr.push(element)
+   }
+ }
 
-throttler()
 
-//Now we have the data inputing at its maximum capacity, we can input the ENOTFOUND errors back into the array. 
-//Next I need to work out how to direct the traffic to the right end. 
+ var throttler = function (arr, i) {
+   if (i < arr.length) {
+     makeCalls(arr[i], arr)
+     setTimeout(function () {
+       throttler(arr, i)
+     }, rps);
+
+     i++;
+   }
+ }
+
+ throttler(alphabet, 0);
