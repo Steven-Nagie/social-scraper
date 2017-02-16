15c18f506f7b5c3f2dabf7131fa0a756b0e915f9
Steven Nagie <sanagie@yahoo.com>
2017-02-16 08:01:06 -0700

    Added 'response' column to exported csv

--- processes/csvExport.js
+++ processes/csvExport.js
@@ -7,7 +7,7 @@ exports.createCSV = (data) => {
   if (fs.existsSync('./public/assets/everything.csv')) {
     fs.unlinkSync('./public/assets/everything.csv');
   }
-  fs.appendFileSync('./public/assets/everything.csv', `Influencer,Followers,URL,Posting Date,Platform,Likes,Shares,Comments,Views\n${data.profiles}`, encoding="utf8");
+  fs.appendFileSync('./public/assets/everything.csv', `Influencer,Followers,URL,Posting Date,Platform,Likes,Shares,Comments,Views,Response\n${data.profiles}`, encoding="utf8");
   // fs.appendFileSync('./public/assets/everything.csv', data.profiles, encoding="utf8");
 
   }
